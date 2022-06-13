d2c4dd120d60118825baa523a2051ec7db3a7ea6
sooappeal <wjdtndk731@gmail.com>
2022-06-13 17:38:41 +0900

    Dog 기능 완성!

--- src/com/kh/git/Hellogit.java
+++ src/com/kh/git/Hellogit.java
@@ -1,7 +1,13 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class Hellogit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!");
+		
+		//Dog 기능 추가
+		Dog dog = new Dog();
+		dog.bark();
 	}
 }
--- src/com/kh/git/animal/Dog.java
+++ src/com/kh/git/animal/Dog.java
@@ -2,4 +2,8 @@ package com.kh.git.animal;
 
 public class Dog {
 
+	public void bark() {
+		System.out.println("멍멍~");
+	}
+	
 }
